bddb939c4a5cffc5ab5a7aefc40cf63946818428
theo191 <65248395+theo191@users.noreply.github.com>
2020-05-13 19:51:39 +0200

    Update Earth.py

--- userbot/plugins/Earth.py
+++ userbot/plugins/Earth.py
@@ -6,13 +6,10 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.pp", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
-	for _ in range(48):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
+	deq = deque(list("PayPal.me/endshop"))
+	
     
