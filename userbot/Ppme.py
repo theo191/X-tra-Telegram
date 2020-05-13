fd52f57e5bc62231fdc93e0c38945188243ad4de
theo191 <65248395+theo191@users.noreply.github.com>
2020-05-13 20:03:10 +0200

    Update and rename userbot/plugins/Kill.py to userbot/Ppme.py

---
+++ userbot/Ppme.py
@@ -0,0 +1,39 @@
+"""COMMAND : .kill"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0
+    
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "ppme":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            
+            "PayPal.me/endshop",
+        ]
+
+        for i in animation_ttl:
+
+
+            await event.edit(animation_chars[i % 103])
