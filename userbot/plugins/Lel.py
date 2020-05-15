8b35f097b57804156cd6102d1e7b04b1ccf762d1
theo191 <65248395+theo191@users.noreply.github.com>
2020-05-15 15:28:52 +0200

    Update and rename Kill.py to Lel.py

---
+++ userbot/plugins/Lel.py
@@ -0,0 +1,41 @@
+"""COMMAND : .lel"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 30
+    
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "lel":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "╱┏┓╱╱╱╭━━━╮┏┓╱╱
+╱┃┃╱╱╱┃╭━╮┃┃┃╱
+╱┃┗━━┓┃╰━╯┃┃┗━━ 
+╱┗━━━┛╰━━━╯┗━━━┛╱",
+        ]
+
+        for i in animation_ttl:
+
+
+            await event.edit(animation_chars[i % 103])
